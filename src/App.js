8ceb4c9c3c28d2a37014c71e094128f9bf624096
Mustafa Anas <khmostafa4@gmail.com>
2019-09-30 02:51:17 +0300

    Setup Bulma

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './bulma.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,6 +18,10 @@ function App() {
           Learn React
         </a>
       </header>
+      <div className="columns"> 
+        <div className="column"></div>
+        <div className="column"></div>
+      </div>
     </div>
   );
 }
