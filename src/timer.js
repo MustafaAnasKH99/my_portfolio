f2117787e45104e5c68e889e878e55e88e50f178
Mustafa Anas <khmostafa4@gmail.com>
2019-09-30 05:26:21 +0300

    Javascript default

--- src/timer.js
+++ src/timer.js
@@ -4,7 +4,7 @@ class Timer extends Component {
     constructor(props) {
       super(props);
         this.state = {
-          do: 'Front End Development'
+          do: 'Javascript'
         };
     }
 
