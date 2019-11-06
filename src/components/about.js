74a6177f1ab33372dee31ee9b9921a3019a97d53
Mustafa Anas <khmostafa4@gmail.com>
2019-11-05 21:16:20 -0500

    emojis <3

--- src/components/about.js
+++ src/components/about.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 import Timer from './timer'
 
 class About extends Component {
@@ -7,16 +6,18 @@ class About extends Component {
         super(props);
         this.state = {  }
     }
-    render() { 
+    render() {
+          
+             
         return ( 
             <div className="about-container">
-                <h1 className="title" id="my-profile-name">Mustafa,</h1>
+                <h1 className="title" id="my-profile-name">Mustafa 👽</h1>
                 <h2 className="subtitle"><Timer /></h2>
                 <p className="about-me-par">
-                I am a passionate and self-driven Junior Full-Stack developer. I have experience building various web platforms and systems ranging from chatapps to learning/contement management systems. I have deep appreciation and love for the open source communtiy and I try my best to help (Hit me up in you need any!)
+                I am a passionate and self-driven Junior Full-Stack developer. I have experience building various web platforms and systems ranging from chatapps to learning/contement management systems. I have deep appreciation 💟 for the open source communtiy and I try my best to help (Hit me up in you need any!)
                 </p>
                 <br />
-                <a className="button is-primary is-large is-outlined" href="mailto:me@mustafaanas.com?Subject=Job%20Opportunity" target="_top"><strong>Hire Me!</strong></a>
+                <a className="button is-primary is-large is-outlined" href="mailto:me@mustafaanas.com?Subject=Job%20Opportunity" target="_top"><strong>Hire Me 🚀 </strong></a>
             </div>
          );
     }
